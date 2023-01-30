100e745e7e8f5e86fb617521427cc8fd4290d292
Theo Diamandis <tjdiamandis@gmail.com>
2023-01-30 14:36:26 -0500

    update univ3 example

--- examples/Univ3.jl
+++ examples/Univ3.jl
@@ -6,26 +6,38 @@ This example illustrates how to setup uniswapv3 pools
 using CFMMRouter
 using LinearAlgebra, SparseArrays, StaticArrays
 
+
+## UniV3 pool parameters
+current_price = 15.0
+lower_ticks = [30.0, 20, 10, 5]
+liquidity = [1.0, 2.0, 1.5, 0.0]
+Ai = [1, 2]
+γ = 0.997
+
+## Create pool
+cfmm = UniV3(current_price, lower_ticks, liquidity, γ, Ai)
+
+
 #=
- The price in each tick refers to the lower bound on the interval
- so the intervals are [t_i,t_i+1] with liquidity L_i
+We find arbitrage assuming a true price of 25.0 (ν1/ν2 = 25.0) and display
+the results.
 =#
+Δ = zeros(2)
+Λ = zeros(2)
+find_arb!(Δ, Λ, cfmm, [25.0, 1.0])
 
-tick_list = [Dict("price" => .1, "liquidity" => 1.0),
-             Dict("price" => .5, "liquidity" => 1.0),
-             Dict("price" => 1.0, "liquidity" => 100.0),
-             Dict("price" => 2.0, "liquidity" => 1.0),
-             Dict("price" => 4.0, "liquidity" => 1.0)]
-
-pool = UniV3([100,100],1,[1,2],1.0,3,tick_list)
-Δ = [0.0,0.0]
-Λ = [0.0,0.0]
-
-find_arb!(Δ,Λ,pool, [1.0,2.0])
-
-print(Δ,Λ)
-
-update_reserves!(pool, Δ, Λ, [1.0,2.0])
-
-println(pool.current_price, pool.current_tick_index)
-
+## Print individual trades
+tokens = Ai
+println("\tTendered basket:")
+for (ind, δ) in enumerate(Δ)
+    if δ > eps()
+        print("\t  $(tokens[ind]): $(round(δ, digits=3)), ")
+    end
+end
+println("\n\tRecieved basket:")
+for (ind, λ) in enumerate(Λ)
+    if λ > eps()
+        print("\t  $(tokens[ind]): $(round(λ, digits=3)), ")
+    end
+end
+print("\n")
